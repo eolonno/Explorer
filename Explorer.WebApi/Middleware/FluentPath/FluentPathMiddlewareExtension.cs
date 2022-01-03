0a1fd00c54a0054f879ca22dd5e4fcd78f322f62
Yegor <yegor.l@mail.ru>
2022-01-03 14:55:36 +0300

    Add fluent path for directory controller

---
+++ Explorer.WebApi/Middleware/FluentPath/FluentPathMiddlewareExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace Explorer.WebApi.Middleware.FluentPath
+{
+    using Microsoft.AspNetCore.Builder;
+
+    public static class FluentPathMiddlewareExtension
+    {
+        public static IApplicationBuilder UseFluentPath(
+            this IApplicationBuilder builder, string forController) =>
+            builder.UseMiddleware<FluentPathMiddleware>(forController);
+    }
+}
--- Explorer.WebApi/Startup.cs
+++ Explorer.WebApi/Startup.cs
@@ -3,6 +3,7 @@ namespace Explorer.WebApi
     using System.Reflection;
     using Explorer.Application;
     using Explorer.Application.Mappings;
+    using Explorer.WebApi.Middleware.FluentPath;
     using Explorer.WebApi.Middleware.ServerLoadMonitoring;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
@@ -42,6 +43,7 @@ namespace Explorer.WebApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Explorer.WebApi v1"));
             }
 
+            app.UseFluentPath("Directory");
             app.UseServerLoadMonitoring(this.Configuration);
             app.UseHttpsRedirection();
             app.UseRouting();
